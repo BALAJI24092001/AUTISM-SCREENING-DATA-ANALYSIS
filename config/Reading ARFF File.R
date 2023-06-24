3236d13191503515873b3760cd0ae4b14afe28b6
BALAJI <dbalajivaraprasad@gmail.com>
2023-06-24 17:54:52 +0530

    Pending commits

--- config/Reading ARFF File.R
+++ config/Reading ARFF File.R
@@ -1,6 +1,11 @@
 library(foreign) # import foreign package to get the read.arff function
-data = read.arff(file = './components/Autism-Adult-Data.arff')
-View(data) # View data in a new window in R-Studio
+data1 = read.arff(file = file.choose())
+View(data1) # View data in a new window in R-Studio
+write.csv(data1, file="vertebral_column.csv")
+
+data2 = read.arff(file = file.choose())
+View(data2) # View data in a new window in R-Studio
+write.csv(data2, file="vertebral.csv")
 
 # Notes:
 # 1. read.arff() : reads data from Weka Attribute-Relation File Format (ARFF) files.
